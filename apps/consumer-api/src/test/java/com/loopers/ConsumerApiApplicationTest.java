ba80185c1a18a6b144dad1c6bf6470677cba5828
si.byun <sangil8585@naver.com>
2025-09-04 15:10:07 +0900

    feat: 컨슈머 앱 추가

---
+++ apps/consumer-api/src/test/java/com/loopers/ConsumerApiApplicationTest.java
@@ -0,0 +1,15 @@
+package com.loopers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ConsumerApiApplicationTest {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+}
